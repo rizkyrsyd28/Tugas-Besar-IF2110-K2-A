32faf075d161def81957295a17859a5e70c48771
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2022-10-24 19:39:16 +0700

    Update time.c

--- Src/time.c
+++ Src/time.c
@@ -1,108 +1,131 @@
-//Nama : Shelma Salsabila
-//NIM : 1352115
-//Tanggal : 06-09-2022
-//Deskripsi : Implementasi time.c
-
 #include <stdio.h>
 #include "time.h"
+#include "boolean.h"
 
-boolean IsTIMEValid (int H, int M, int S){
-    return (0<=H) && (H<24) && (0<=M) && (M<60) && (0<=S) && (S<60);
+/* ***************************************************************** */
+/* DEFINISI PRIMITIF                                                 */
+/* ***************************************************************** */
+/* KELOMPOK VALIDASI TERHADAP TYPE                                   */
+/* ***************************************************************** */
+boolean IsTIMEValid (int D,int H,int M){
+	return ((D>= 0 && D <= 30) && (H >= 0 && H <= 23) && (M >= 0 && M <= 59));
 }
-void CreateTime (TIME * T, int HH, int MM, int SS)
-{
+/* Mengirim true jika H,M,S dapat membentuk T yang valid */
+/* dipakai untuk mentest SEBELUM membentuk sebuah Jam */
 
-    Hour(*T)=HH;
-    Minute(*T)=MM;
-    Second(*T)=SS;
-    
+/* *** Konstruktor: Membentuk sebuah TIME dari komponen-komponennya *** */
+void CreateTime (TIME * T, int DD, int HH, int MM) {
+	Day(*T) = DD;
+	Hour(*T) = HH;
+	Minute(*T) = MM;
+}
+/* Membentuk sebuah TIME dari komponen-komponennya yang valid */
+/* Prekondisi : HH, MM, SS valid untuk membentuk TIME */
+
+/* ***************************************************************** */
+/* KELOMPOK BACA/TULIS                                               */
+/* ***************************************************************** */
+void BacaTIME (TIME * T) {
+	int HH, MM, DD;
+	do {
+		scanf("%d %d %d", &DD, &HH, &MM);
+	} while (!IsTIMEValid(DD,HH,MM) && printf("Jam tidak valid\n"));
+	CreateTime(T, DD, HH, MM);
 }
 
-void BacaTIME (TIME * T){
-    boolean benar=false;
-    int j,m,d;
-    while(benar == false){
-        scanf("%d %d %d", &j,&m,&d);
-        benar=IsTIMEValid(j,m,d);
-        if (benar == false){
-            printf("Jam tidak Valid\n");
-        }
-    }
-    CreateTime(T,j,m,d);
-}
-
-void TulisTIME (TIME T){
-   printf("%02d:%02d:%02d", Hour(T), Minute(T), Second(T));
-}
-
-long TIMEToDetik (TIME T){
-    return (3600*Hour(T)+ 60*Minute(T) + Second(T));
-}
-
-TIME DetikToTIME (long N){
-	int detik; 
-	TIME T;
-
-	if (N>=86400) {
-		detik = N % 86400;
-	} else {
-		detik = N;
+   
+void TulisTIMEKadaluarsa (TIME T) {
+	if(Day(T) != 0 && Hour(T) != 0 && Minute(T) != 0)
+	{
+		printf("%d hari, %d jam, %d menit", Day(T), Hour(T), Minute(T));
 	}
-
-	Hour(T) = detik/3600;
-	Minute(T) = (detik % 3600)/60 ; 
-	Second(T) = (detik % 3600)%60;
-	return T;
+	else if(Day(T) == 0 && Hour(T) != 0 && Minute(T) != 0)
+	{
+		printf("%d jam, %d menit", Hour(T), Minute(T));
+	}
+	else if(Day(T) == 0 && Hour(T) == 0 && Minute(T) != 0)
+	{
+		printf("%d menit", Minute(T));
+	}
+	else if(Day(T) != 0 && Hour(T) == 0 && Minute(T) != 0)
+	{
+		printf("%d hari,%d menit", Day(T),Minute(T));
+	}
+	else if(Day(T) == 0 && Hour(T) != 0 && Minute(T) == 0)
+	{
+		printf("%d jam", Hour(T));
+	}
+	else if(Day(T) != 0 && Hour(T) == 0 && Minute(T) == 0)
+	{
+		printf("%d hari", Day(T));
+	}
+	else if(Day(T) != 0 && Hour(T) != 0 && Minute(T) == 0)
+	{
+		printf("%d hari, %d jam", Day(T), Hour(T), Minute(T));
+	}
+	else if(Day(T) == 0 && Hour(T) == 0 && Minute(T) == 0)
+	{
+		printf("Kadaluarsa");
+	}
+	
+	
 }
 
-boolean TEQ (TIME T1, TIME T2){
-    return (Hour(T1)==Hour(T2)) && (Minute(T1)==Minute(T2)) && (Second(T1)==Second(T2));
+void TulisTIMEDelivery (TIME T)
+{
+	printf("(%d.%d)",Hour(T),Minute(T));
 }
 
-boolean TNEQ (TIME T1, TIME T2){
-    return!((Hour(T1) == Hour(T2)) && (Minute(T1) == Minute(T2)) && (Second(T1) == Second(T2)));
+
+TIME NextMinute (TIME T) {
+	TIME t;
+	t = T;
+	Minute(t) += 1;					
+	if (Minute(t) > 59) {
+		Minute(t) = 0;
+		Hour(t) += 1;
+		if (Hour(t) > 23) {
+			Hour(t) = 0;
+			Day(t) += 1;
+			//Diasumsikan hari tidak akan lebih dari 30 karena mengingat ini waktu kadaluarsa suatu makanan
+			}
+		}
+	return t;
 }
 
-boolean TLT (TIME T1, TIME T2){
-    return TIMEToDetik(T1)<TIMEToDetik(T2);
+
+TIME NextNMinute (TIME T, int N) {
+	int i; TIME t;
+	t = T;
+	for (i = 0; i < N; i++) {
+		t = NextMinute(t);
+	}
+	return t;
 }
 
-boolean TGT (TIME T1, TIME T2){
-    return TIMEToDetik(T1)>TIMEToDetik(T2);
-}
 
-TIME NextDetik (TIME T){
-    long detik;
-    detik = TIMEToDetik(T);
-    detik = detik+1 ;
-    return DetikToTIME(detik);
+TIME PrevMinute (TIME T) {
+	TIME t;
+	t = T;
+	Minute(t) -= 1;
+	if (Minute(t) < 0) {
+		Minute(t) = 59;
+		Hour(t) -= 1;
+		if (Hour(t) < 0) {
+			Hour(t) = 23;
+			Day(t) -= 1;
+			//Diasumsikan juga hari tidak akan kurang dari 0 
+		}
+	}
+	return t;
 }
+/* Mengirim 1 detik sebelum T dalam bentuk TIME */
 
-TIME NextNDetik (TIME T, int N){
-    long detik;
-    detik = TIMEToDetik(T);
-    detik = detik + N;
-    return DetikToTIME(detik);
-}
-
-TIME PrevDetik (TIME T){
-    long detik;
-    detik = TIMEToDetik(T);
-    detik = detik-1 ;
-    return DetikToTIME(detik);
-}
-
-TIME PrevNDetik (TIME T, int N){
-    long detik;
-    detik = TIMEToDetik(T);
-    detik = detik-N ;
-    return DetikToTIME(detik);
-}
-
-long Durasi (TIME TAw, TIME TAkh){
-    long selisih = TIMEToDetik(TAkh) - TIMEToDetik(TAw);
-    if (selisih < 0) {
-        selisih += 86400;
-    }
-    return selisih;
+TIME PrevNDetik (TIME T, int N) {
+	int i; TIME t;
+	t = T;
+	for (i = 0; i < N; i++) {
+		t = PrevMinute(t);
+	}
+	return t;
 }
