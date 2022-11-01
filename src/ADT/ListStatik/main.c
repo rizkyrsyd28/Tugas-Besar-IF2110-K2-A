433c04d44cccc3c75e52a69494b770a485b768f7
Gulilil <juan.csantoso@gmail.com>
2022-11-01 20:41:26 +0700

    revising main

--- src/ADT/ListStatik/main.c
+++ src/ADT/ListStatik/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "liststatik.h"
+#include "liststatik.c"
 
 int main() {
 
     ListStatik a;
     CreateListStatik(&a);
-    Makanan uwa = {19, {"MyFood", 6}, {23, 21, 9}, {"MyAction", 8}, {23, 21, 9}};
+    Makanan uwa = {19, {"MyFood", 6}, {23, 21, 9}, {"MyAction", 8}, {1,1}, {23, 21, 9}};
 
     if (isEmpty(a)) {
         printf("This is empty\n");
