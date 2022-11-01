903ff504e78d5afc5015d092a216ffa0123f40e3
Zaki <zakiamanullah215@gmail.com>
2022-11-01 20:33:23 +0700

    some driver

--- src/ADT/ListStatik/main.c
+++ src/ADT/ListStatik/main.c
@@ -6,12 +6,17 @@ int main() {
     ListStatik a;
     CreateListStatik(&a);
     Makanan uwa = {19, {"MyFood", 6}, {23, 21, 9}, {"MyAction", 8}, {23, 21, 9}};
+    Makanan uwb = {19, {"MyFood", 6}, {23, 21, 9}, {"MyAction", 8}, {23, 21, 9}};
+    Makanan trash;
 
     if (isEmpty(a)) {
         printf("This is empty\n");
     }
 
     insertFirst(&a, uwa);
+    insertFirst(&a, uwb);
+    printf("%d\n", listLength(a));
+    deleteFirst(&a, &trash);
     printf("%d\n", listLength(a));
 
     return 0;
